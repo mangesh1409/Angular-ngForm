67d76c23ad7c83e8e1a365027ccccf548e15a39b
Mangesh Jadhav <149mpj@gmail.com>
2020-07-19 20:05:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormWithngForm';
+  Name;  Email;  Number;
+
+  Submitdata(profileForm){
+      // console.log(profileForm);
+      // this.Name=profileForm.controls.name.value;
+      // this.Email=profileForm.controls.email.value;
+      // this.Number=profileForm.controls.mobile.value;
+      this.Name=profileForm.name;
+      this.Email=profileForm.email;
+      this.Number=profileForm.mobile;
+      profileForm.reset();
+  }
+
 }
